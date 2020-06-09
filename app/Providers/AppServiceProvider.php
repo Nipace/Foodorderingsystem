3c93fab9508070647c1f249cc35486111c96ee53
Nipesh <nipesh12pant@outlook.com>
2020-06-09 21:11:53 +0545

    appservice provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(env('REDIRECT_HTTPS'))
+        {
+            $url->forceSchema('https');
+        }
     }
 }
